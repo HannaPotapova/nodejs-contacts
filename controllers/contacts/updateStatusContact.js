d6a3950d30ce4ebc11a015d0bdbb5e83eb8a45bf
Hanna Potapova <an.al.potapova@gmail.com>
2023-02-09 14:09:08 +0200

    add updateStatus

---
+++ controllers/contacts/updateStatusContact.js
@@ -0,0 +1,28 @@
+const { Contact } = require("../../models");
+const { NotFound } = require("http-errors");
+
+const updateStatusContact = async (req, res) => {
+  const { contactId } = req.params;
+  const { favorite } = req.body;
+  const result = await Contact.findByIdAndUpdate(
+    contactId,
+    { favorite },
+    {
+      new: true,
+    }
+  );
+  if (!result) {
+    throw new NotFound(`Contact with id=${contactId} not found`);
+  }
+  res.json({
+    status: "success",
+    code: 200,
+    data: {
+      result,
+    },
+  });
+};
+
+module.exports = updateStatusContact;
+
+// /\(?\d{3}\)?([-])\d{3}\1\d{2}1\d{2}/
